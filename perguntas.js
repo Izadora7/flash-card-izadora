7369ebab0244c55837f43d00d419a4281a450ac1
Iza <izadora.souza.silva@escola.pr.gov.br>
2025-07-30 12:22:16 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'Qual a capital do canadá?',
+    'A capital do canadá é Ottawa'
+)
+
+criaCartao(
+    'Matematica',
+    'Oque é uma equação de 1 grau?',
+    'É uma equação com incógnita elevado à primeira potência'
+)
+
+criaCartao(
+    'Geografia',
+    'Oque é latitude?',
+    'Distẫncia em graus da linha do Equador'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'oque é algoritmo?',
+    'Conjunto de instruções ordenadas para resolver um problema'
+)
